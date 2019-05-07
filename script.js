7df1b622702328c345a09921e370f7f6bb6f5fed
rayadajani <37849258+rayadajani@users.noreply.github.com>
2019-05-07 14:46:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function goCom(el){
+	document.location.href="https://macielegaspi.github.io/gulagescape/gulagescape/";
+}
+function goMakeup(el){
+	document.location.href="https://kobywu.github.io/makeuproom/caitlyn.html";
+}
+function goSports(el){
+	document.location.href="https://zoraizhashmi.github.io/sportsescape/";
+}
+function goZombie(el){
+	document.location.href="https://andresvilla1.github.io/AndresVP8/ ";
+}
+function goToonz(el){
+	document.location.href="https://sambiner.github.io/escaperoom/esc_rm.html";
+}
+function goSpace(el){
+	document.location.href="https://leahtorres.github.io/phase9/index1.html";
+}
