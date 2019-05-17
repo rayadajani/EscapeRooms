fc4485447ec80e1b7a153d8788e4c6a9967647ce
rayadajani <37849258+rayadajani@users.noreply.github.com>
2019-05-17 12:55:24 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+a = document.getElementById("space");
+b = document.getElementById("sports");
+c = document.getElementById("makeup");
+d = document.getElementById("toonz");
+e = document.getElementById("zombie");
+f = document.getElementById("communism");
 function goCom(el){
 	document.location.href="https://macielegaspi.github.io/gulagescape/gulagescape/";
 }
@@ -16,3 +22,82 @@ function goToonz(el){
 function goSpace(el){
 	document.location.href="https://leahtorres.github.io/phase9/index1.html";
 }
+function escapeSpace(el){
+	if (el.src.match("images/escape_space4.PNG")){
+		el.src = "images/escape_space1.PNG";
+	} else if (el.src.match ("images/escape_space1.PNG")){
+		el.src = "images/escape_space2.PNG";
+	} else if (el.src.match ("images/escape_space2.PNG")){
+		el.src = "images/escape_space4.PNG";
+	} else if (el.src.match ("images/escape_space4.PNG")){
+		el.src = "images/escape_space1.PNG";
+	} else {
+		// do nothing
+	}
+}
+function escapeSports(el){
+	if (el.src.match("images/sports_escape4.PNG")){
+		el.src = "images/sports_escape1.PNG";
+	} else if (el.src.match ("images/sports_escape1.PNG")){
+		el.src = "images/sports_escape2.PNG";
+	} else if (el.src.match ("images/sports_escape2.PNG")){
+		el.src = "images/sports_escape3.PNG";
+	} else if (el.src.match ("images/sports_escape3.PNG")){
+		el.src = "images/sports_escape4.PNG";
+	} else {
+		// do nothing
+	}
+}
+function escapeMakeup(el){
+	if (el.src.match("images/makeup_room3.PNG")){
+		el.src = "images/makeup_room1.PNG";
+	} else if (el.src.match ("images/makeup_room1.PNG")){
+		el.src = "images/makeup_room2.PNG";
+	} else if (el.src.match ("images/makeup_room2.PNG")){
+		el.src = "images/makeup_room3.PNG";
+	} else if (el.src.match ("images/makeup_room3.PNG")){
+		el.src = "images/makeup_room1.PNG";
+	} else {
+		// do nothing
+	}
+}
+function escapeToonz(el){
+	if (el.src.match("images/toonz6.PNG")){
+		el.src = "images/toonz3.PNG";
+	} else if (el.src.match ("images/toonz3.PNG")){
+		el.src = "images/toonz4.PNG";
+	} else if (el.src.match ("images/toonz4.PNG")){
+		el.src = "images/toonz5.PNG";
+	} else if (el.src.match ("images/toonz5.PNG")){
+		el.src = "images/toonz6.PNG";
+	} else {
+		// do nothing
+	}
+}
+function escapeZombies(el){
+	if (el.src.match("images/zombie_escape3.PNG")){
+		el.src = "images/zombie_escape1.PNG";
+	} else if (el.src.match ("images/zombie_escape1.PNG")){
+		el.src = "images/zombie_escape2.PNG";
+	} else if (el.src.match ("images/zombie_escape2.PNG")){
+		el.src = "images/zombie_escape3.PNG";
+	} else if (el.src.match ("images/zombie_escape3.PNG")){
+		el.src = "images/zombie_escape1.PNG";
+	} else {
+		// do nothing
+	}
+}
+function escapeCommunism(el){
+	if (el.src.match("images/escape_c5.PNG")){
+		el.src = "images/escape_c2.PNG";
+	} else if (el.src.match ("images/escape_c2.PNG")){
+		el.src = "images/escape_c3.PNG";
+	} else if (el.src.match ("images/escape_c3.PNG")){
+		el.src = "images/escape_c4.PNG";
+	} else if (el.src.match ("images/escape_c4.PNG")){
+		el.src = "images/escape_c5.PNG";
+	} else {
+		// do nothing
+	}
+}
+ 
